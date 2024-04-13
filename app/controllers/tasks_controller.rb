bc68b498942c7c395632854e9b89ed808e845304
Leonardo Acuña V <Leonardoacuavilla@gmail.com>
2024-04-12 23:43:13 -0500

    se agrego linea de codigos para su mejora

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,17 @@
-
 class TasksController < ApplicationController
   def fetch_earthquake_data_task
-  # Rake::Task['fetch_earthquake_data_task:perform'].invoke
-  system('rake fetch_earthquake_data_task:perform')
+    # Cargar todas las tareas de Rake definidas en la aplicación
+    Rails.application.load_tasks
+    # Llamar directamente a la tarea de rake desde Ruby
+    Rake::Task['fetch_earthquake_data_task:perform'].invoke
 
-    # Por ejemplo, puedes llamar a un método en un modelo específico que maneje la lógica de la tarea.
-    # Aquí solo proporciono un ejemplo básico:
-    #EarthquakeDataFetcher.fetch_data
-    
+    # O si prefieres utilizar Active Job, puedes encolar el trabajo asíncronamente
+    # EarthquakeDataFetcherJob.perform_later
+
     # Responder con un mensaje de éxito o cualquier otra respuesta apropiada
     render json: { message: 'La tarea fetch_earthquake_data_task se ha ejecutado exitosamente' }
+  rescue => e
+    # Manejar cualquier error que pueda ocurrir durante la ejecución de la tarea
+    render json: { error: 'Se produjo un error al ejecutar la tarea: ' + e.message }, status: :internal_server_error
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  # Ruta para ejecutar la tarea de obtener datos de terremotos
+  get '/fetch_earthquake_data', to: 'tasks#fetch_earthquake_data_task'
 
   # Defines the root path route ("/")
   # root "posts#index"
